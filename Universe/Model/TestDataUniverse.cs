3c2c3497fc1311e01f02cb56f6db056a450c16e6
Severin Anatoliy <severin@pointcad.ru>
2018-12-03 22:43:32 +0300

    add test data

---
+++ Universe/Model/TestDataUniverse.cs
@@ -0,0 +1,30 @@
+﻿namespace Universe.Model
+{
+    public sealed class TestDataUniverse: IUniverse
+    {
+        private readonly IUniverse _universe;
+
+        public TestDataUniverse(IUniverse universe)
+        {
+            _universe = universe;
+            var planets = _universe.Planets();
+            planets.Create("Mercury");
+            planets.Create("Venus");
+            planets.Create("Earth");
+            planets.Create("Mars");
+            planets.Create("Jupiter");
+            planets.Create("Saturn");
+            planets.Create("Uranus");
+        }
+
+        public IPlanets Planets()
+        {
+            return _universe.Planets();
+        }
+
+        public IProperties Properties()
+        {
+            return _universe.Properties();
+        }
+    }
+}

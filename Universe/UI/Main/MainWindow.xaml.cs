6bda21df54674a7b2897b99283c6b2a4935e8067
Severin Anatoliy <severin@pointcad.ru>
2018-12-14 23:05:48 +0300

    add MahApps

--- Universe/UI/Main/MainWindow.xaml.cs
+++ Universe/UI/Main/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace Universe.UI.Main
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow: MetroWindow
     {
         public MainWindow(MainViewModel mainViewModel)
         {

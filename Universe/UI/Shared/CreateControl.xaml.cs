eede92942ec21b60872575680ba5ecf3cb3d094f
Severin Anatoliy <severin@pointcad.ru>
2018-12-15 13:48:37 +0300

    add control to input new object name

---
+++ Universe/UI/Shared/CreateControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Universe.UI.Shared
+{
+    /// <summary>
+    /// Interaction logic for CreateControl.xaml
+    /// </summary>
+    public partial class CreateControl : UserControl
+    {
+        public static readonly DependencyProperty CreateCommandProperty = DependencyProperty.Register(nameof(CreateCommand), typeof(ICommand), typeof(CreateControl), new PropertyMetadata(null));
+
+        public CreateControl()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddButton.Visibility = Visibility.Collapsed;
+            NameTextBox.Visibility = Visibility.Visible;
+            ApplyButton.Visibility = Visibility.Visible;
+        }
+
+        private void ApplyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddButton.Visibility = Visibility.Visible;
+            NameTextBox.Visibility = Visibility.Collapsed;
+            ApplyButton.Visibility = Visibility.Collapsed;
+            if (CreateCommand?.CanExecute(NameTextBox.Text) ?? false)
+            {
+                CreateCommand.Execute(NameTextBox.Text);
+            }
+        }
+
+        public ICommand CreateCommand
+        {
+            get => (ICommand) GetValue(CreateCommandProperty);
+            set => SetValue(CreateCommandProperty, value);
+        }
+
+        private void NameTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            ApplyButton.IsEnabled = !string.IsNullOrEmpty(NameTextBox.Text);
+        }
+    }
+}
